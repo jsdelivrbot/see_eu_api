652d00daa0595545fcb43e8b50bef07963743a92
sahil.khanna <email.sahilkhanna@gmail.com>
2017-11-25 00:20:48 +0530

    Bookings
    1) Listing bookings (itineraries)

--- src/controllers/Itineraries.ts
+++ src/controllers/Itineraries.ts
@@ -1,6 +1,9 @@
 import { Router, Request, Response } from 'express';
 import { Db, ObjectID, InsertOneWriteOpResult } from 'mongodb';
 import { Itineraries } from '../models/itineraries';
+import { ItineraryDetail } from '../models/itinerary-detail';
+import { totalmem } from 'os';
+import { resolve } from 'url';
 
 const ITINERARIES = "itineraries";
 const ITINERARY_DETAILS = "itineraryDetails";
@@ -14,10 +17,10 @@ export class ItinerariesController {
 
         this.router.post('/', this.post.bind(this));
         this.router.get('/:id', this.getById.bind(this));
+        this.router.get('/itineraries/:id', this.itineraries.bind(this));
     }
 
     private post(req: Request, res: Response) {
-        console.log(this.db);
         let itinerary = req.body;
         itinerary.id = (new Date()).valueOf().toString();
         itinerary.test = new Date();
@@ -41,4 +44,83 @@ export class ItinerariesController {
             }).catch(err => res.status(500).send(err));
     }
 
+    private itineraries(req: Request, res: Response) {
+        let itineraries = [];
+        let currentPassengerCount = 0;
+        
+        this.db.collection(ITINERARIES).find().toArray()
+        .then((_itineraries: Itineraries[]) => {
+
+            let currentItineraryCount = 0;
+            const nextItinerary = () => {
+                if (currentItineraryCount < _itineraries.length) {
+                    let _itinerary = _itineraries[currentItineraryCount];
+                    
+                    let itinerary = {
+                        passengers: [],
+                        trip: {}
+                    };
+
+                    _itinerary['itineraryDetails'].forEach(_itineraryDetail => {
+                        itinerary.passengers.push({
+                            name: _itineraryDetail['name'],
+                            gender: _itineraryDetail['gender'],
+                            pickupPoint: _itineraryDetail['pickupPoint']['locality']['name'],
+                            price: _itineraryDetail['pickupPoint']['price']
+                        });
+                    });
+
+                    console.log(_itinerary.tripId);
+
+                    this.db.collection('trips').findOne({
+                        'id': _itinerary.tripId
+                    })
+                    .then(__itinerary => {
+                        console.log('__itinerary: ' + __itinerary);
+                        itinerary.trip = {
+                            name: __itinerary['tripDetails'][0]['name'],
+                            description: __itinerary['tripDetails'][0]['description']
+                        }
+                        itineraries.push(itinerary);
+                        currentItineraryCount++;
+                        nextItinerary();
+                    });
+                }
+                else {
+                    res.json({code: 0, data: itineraries});
+                }
+            }
+
+            nextItinerary();
+        })
+        .catch(_err => {
+            res.send({code: -1, message: _err.toString()})
+        });
+    }
+
+    // private itineraries(req: Request, res: Response) {
+    //     //TODO: Add filter of userid
+    //     this.db.collection(ITINERARIES).aggregate([{
+    //         $lookup: {
+    //             from: "trips",
+    //             localField: "tripId",
+    //             foreignField: "id",
+    //             as: "trip"
+    //         }
+    //     }]).toArray()
+    //     .then((itineraries: Itineraries[]) => {
+    //         console.log(itineraries[0].itineraryDetails[0].pickupPoint.localityId);
+    //         res.send({code: 0, data: itineraries});
+    //     })
+    //     .catch(err => {
+    //         res.send({code: 4000, message: err.toString()})
+    //     });
+    //     // this.db.collection(ITINERARIES).find().toArray()
+    //     // .then((itineraries: Itineraries[]) => {
+    //     //     res.send({code: 0, data: itineraries});
+    //     // })
+    //     // .catch(err => {
+    //     //     res.send({code: 4000, message: err.toString()})
+    //     // });
+    // }
 }
